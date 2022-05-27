aa631868d2001239cfc9cd71c35d11bedeb3abba
Vashu <vmanishkumar04@gmail.com>
2022-05-27 05:11:23 -0700

    sending

--- app.js
+++ app.js
@@ -2,27 +2,27 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 80;
 const axios = require("axios");
-
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("hi");
 });
 app.post("/otp", async (req, res) => {
-  const res = await axios({
+  const response = await axios({
     method: "post",
     url: "http://localhost:3000/api/v1/users/getotp",
     data: req.body,
     withCredentials: true,
   });
-  return res.send(res);
+  return res.send(response.data);
 });
 app.post("/signup", async (req, res) => {
-  const res = await axios({
+  const response = await axios({
     method: "post",
     url: "http://localhost:3000/api/v1/users/signup",
     data: req.body,
     withCredentials: true,
   });
-  return res.send(res);
+  return res.send(response.data);
 });
 app.listen(PORT, () => {
   console.log("start");
