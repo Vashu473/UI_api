ef5324ca4d6ef9789fac6cf5dda9e472475dc712
Vashu <vmanishkumar04@gmail.com>
2022-05-27 04:42:38 -0700

    vhfd

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 80;
+
+app.get("/", (req, res) => {
+  res.send("hi");
+});
+app.listen(PORT, () => {
+  console.log("start");
+});
