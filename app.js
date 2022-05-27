1d740d6126ae2c3393f2d197f74f6cd52f666dc4
Vashu <vmanishkumar04@gmail.com>
2022-05-27 04:59:03 -0700

    dhfvosd

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 80;
+const axios = require("axios");
 
 app.get("/", (req, res) => {
   res.send("hi");
+});
+app.post("/otp", async (req, res) => {
+  const res = await axios({
+    method: "post",
+    url: "http://localhost:3000/api/v1/users/getotp",
+    data: req.body,
+    withCredentials: true,
+  });
+  return res.send(res);
+});
+app.post("/signup", async (req, res) => {
+  const res = await axios({
+    method: "post",
+    url: "http://localhost:3000/api/v1/users/signup",
+    data: req.body,
+    withCredentials: true,
+  });
+  return res.send(res);
 });
 app.listen(PORT, () => {
   console.log("start");
